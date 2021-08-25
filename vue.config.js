ec3b9ea6de2a2812fc87df20d5f7f3c08086a778
JarryChung <jarrychung@outlook.com>
2021-08-25 23:38:25 +0800

    chore(task): ypdate release config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: './',
+
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
     types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)));
